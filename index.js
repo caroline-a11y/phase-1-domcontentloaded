237999e02dbc4ba150ce2874728f83edcd9b14c4
Caroline <caroline.syokau@student.moringaschool.com>
2022-11-29 21:05:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let paragraph = document.getElementById('text')
+    paragraph.textContent = 'This is really cool!'
+  });
+  
+  
+  
+  
